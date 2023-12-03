5d72bd4535d1f8490b1d31f46d17e82aa3a27e1c
Monish Reddy Kotturu <monishkotturu@gmail.com>
2023-12-03 03:21:52 -0500

    refactored code

--- day3/part2.py
+++ day3/part2.py
@@ -1,78 +1,77 @@
 from collections import defaultdict
 import math
 
-schematic = []
+def main(file):
+	schematic = []
 
-while True:
-	try:
-		schematic.append(input())
-	except EOFError:
-		break
+	lines = file.read().splitlines()
+	for line in lines:
+		schematic.append(line)
 
-gears = defaultdict(list)
+	gears = defaultdict(list)
 
-for line_idx, line in enumerate(schematic):
-	is_part_number = False
-	temp_num = ''
-	gear = None
+	for line_idx, line in enumerate(schematic):
+		is_part_number = False
+		temp_num = ''
+		gear = None
 
-	for char_idx, char in enumerate(line):
-		if char.isdigit():
-			temp_num += char
+		for char_idx, char in enumerate(line):
+			if char.isdigit():
+				temp_num += char
 
-			if char_idx == len(line) - 1:
+				if char_idx == len(line) - 1:
+					if is_part_number:
+						gears[gear].append(int(temp_num))
+					is_part_number = False
+					temp_num = ''
+
+				if is_part_number:
+					continue
+
+				if char_idx > 0:
+					if schematic[line_idx][char_idx - 1] == '*':
+						is_part_number = True
+						gear = (line_idx, char_idx - 1)
+						continue
+				if char_idx < len(line) - 1:
+					if schematic[line_idx][char_idx + 1] == '*':
+						is_part_number = True
+						gear = (line_idx, char_idx + 1)
+						continue
+				if line_idx > 0:
+					if schematic[line_idx - 1][char_idx] == '*':
+						is_part_number = True
+						gear = (line_idx - 1, char_idx)
+						continue
+				if line_idx < len(schematic) - 1:
+					if schematic[line_idx + 1][char_idx] == '*':
+						is_part_number = True
+						gear = (line_idx + 1, char_idx)
+						continue
+				if char_idx > 0 and line_idx > 0:
+					if schematic[line_idx - 1][char_idx - 1] == '*':
+						is_part_number = True
+						gear = (line_idx - 1, char_idx - 1)
+						continue
+				if char_idx < len(line) - 1 and line_idx > 0:
+					if schematic[line_idx - 1][char_idx + 1] == '*':
+						is_part_number = True
+						gear = (line_idx - 1, char_idx + 1)
+						continue
+				if char_idx > 0 and line_idx < len(schematic) - 1:
+					if schematic[line_idx + 1][char_idx - 1] == '*':
+						is_part_number = True
+						gear = (line_idx + 1, char_idx - 1)
+						continue
+				if char_idx < len(line) - 1 and line_idx < len(schematic) - 1:
+					if schematic[line_idx + 1][char_idx + 1] == '*':
+						is_part_number = True
+						gear = (line_idx + 1, char_idx + 1)
+						continue
+			else:
 				if is_part_number:
 					gears[gear].append(int(temp_num))
 				is_part_number = False
 				temp_num = ''
 
-			if is_part_number:
-				continue
-
-			if char_idx > 0:
-				if schematic[line_idx][char_idx - 1] == '*':
-					is_part_number = True
-					gear = (line_idx, char_idx - 1)
-					continue
-			if char_idx < len(line) - 1:
-				if schematic[line_idx][char_idx + 1] == '*':
-					is_part_number = True
-					gear = (line_idx, char_idx + 1)
-					continue
-			if line_idx > 0:
-				if schematic[line_idx - 1][char_idx] == '*':
-					is_part_number = True
-					gear = (line_idx - 1, char_idx)
-					continue
-			if line_idx < len(schematic) - 1:
-				if schematic[line_idx + 1][char_idx] == '*':
-					is_part_number = True
-					gear = (line_idx + 1, char_idx)
-					continue
-			if char_idx > 0 and line_idx > 0:
-				if schematic[line_idx - 1][char_idx - 1] == '*':
-					is_part_number = True
-					gear = (line_idx - 1, char_idx - 1)
-					continue
-			if char_idx < len(line) - 1 and line_idx > 0:
-				if schematic[line_idx - 1][char_idx + 1] == '*':
-					is_part_number = True
-					gear = (line_idx - 1, char_idx + 1)
-					continue
-			if char_idx > 0 and line_idx < len(schematic) - 1:
-				if schematic[line_idx + 1][char_idx - 1] == '*':
-					is_part_number = True
-					gear = (line_idx + 1, char_idx - 1)
-					continue
-			if char_idx < len(line) - 1 and line_idx < len(schematic) - 1:
-				if schematic[line_idx + 1][char_idx + 1] == '*':
-					is_part_number = True
-					gear = (line_idx + 1, char_idx + 1)
-					continue
-		else:
-			if is_part_number:
-				gears[gear].append(int(temp_num))
-			is_part_number = False
-			temp_num = ''
-
-print(sum([math.prod(gears[key]) for key in gears.keys() if len(gears[key]) == 2]))
+	print(sum([math.prod(gears[key]) for key in gears.keys() if len(gears[key]) == 2]))
