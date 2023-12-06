cac33177cbb1375faed82c419ee5abc2e50df58e
Monish Reddy Kotturu <monishkotturu@gmail.com>
2023-12-06 01:26:59 -0500

    day 6 part 2 with njit

--- day6/part2.py
+++ day6/part2.py
@@ -1,18 +1,21 @@
-import numpy as np
+from numba import njit
 
-def main(file):
-	lines = file.read().splitlines()
-
-	_, time = lines[0].split(':')
-	time = int(''.join([x for x in time.strip() if x != ' ']))
-
-	_, distance = lines[1].split(':')
-	distance = int(''.join([x for x in distance.strip() if x != ' ']))
-
+@njit
+def njit_run(time, distance):
 	num_ways = 0
 	for _t in range(1, time + 1):
 		possible_dist = _t * (time - _t)
 		if possible_dist > distance:
 			num_ways += 1
 
-	print(num_ways)
+	return num_ways
+
+def main(file):
+	lines = file.read().splitlines()
+	_, time = lines[0].split(':')
+	time = int(''.join([x for x in time.strip() if x != ' ']))
+
+	_, distance = lines[1].split(':')
+	distance = int(''.join([x for x in distance.strip() if x != ' ']))
+
+	print(njit_run(time, distance))
